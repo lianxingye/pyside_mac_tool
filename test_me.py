af093e5b914d9da42ee81fe29ca29e0a55f9d81a
tosoth <tosoth@gmail.com>
2018-12-05 01:27:00 +0900

    fix

--- test_me.py
+++ test_me.py
@@ -2,10 +2,28 @@ import json
 import requests
 from pprint import pprint
 
-with open('/Users/tosoth/Projects/bitbucket/trv_chef/1') as f:
+#curl -sb -H "Accept: application/json" -u tosoth:123123 http://localhost:7990/rest/api/1.0/projects/TRVCHEF/repos/trv_chef/pull-requests/1/activities > /tmp/1
+def get_pr_detail(my_id):
+      auth='tosoth:123123'
+      url='http://localhost:7990/rest/api/1.0/projects/TRVCHEF/repos/trv_chef/pull-requests/'+str(my_id)+'/activities'
+      headers={'Content-type': 'application/json'}
+      r = requests.get(url, headers=headers, auth=('tosoth', '123123'))
+      with open('/tmp/1','w') as f:
+        f.write(r.text)
+      r.close()
+
+def handle(my_id):
+  with open('/tmp/'+str(my_id)) as f:
     data = json.load(f)
 
-values=data["values"]
+  values=data["values"]
+  for i in values:
+   if i[u'action'] == u'COMMENTED':
+    replies = i[u'comment'][u'comments']
+    if replies == []:
+      print "BINGO!"
+      id=i[u'comment'][u'id']
+      reply(id, "test done1")
 
 def reply(id, content):
       auth='tosoth:123123'
@@ -16,10 +34,31 @@ def reply(id, content):
       print r
       r.close()
 
-for i in values:
-  if i[u'action'] == u'COMMENTED':
-    replies = i[u'comment'][u'comments']
-    if replies == []:
-      print "BINGO!"
-      id=i[u'comment'][u'id']
-      reply(id, "test done")
+def get(url):
+      url='http://localhost:7990/rest/api/1.0/projects/TRVCHEF/repos/trv_chef/pull-requests'
+      headers={'Content-type': 'application/json'}
+      r = requests.get(url, headers=headers, auth=('tosoth', '123123'))
+      r.close()
+
+
+
+if True:
+      url='http://localhost:7990/rest/api/1.0/projects/TRVCHEF/repos/trv_chef/pull-requests'
+      headers={'Content-type': 'application/json'}
+      r = requests.get(url, headers=headers, auth=('tosoth', '123123'))
+      pr_text = r.text
+      r.close()
+
+with open('/tmp/pr','w') as f:
+    f.write(pr_text)
+
+with open('/tmp/pr') as f:
+    data_pr = json.load(f)
+
+for i in data_pr["values"]:
+  pr_content=i[u'description']
+  pr_id=i[u'id']
+  print pr_id
+  get_pr_detail(pr_id)
+  handle(pr_id)
+  
--- test_me.sh
+++ test_me.sh
@@ -1,3 +1,7 @@
-curl -sb -H "Accept: application/json" -u tosoth:123123 http://localhost:7990/rest/api/1.0/projects/TRVCHEF/repos/trv_chef/pull-requests/1/activities > /Users/tosoth/Projects/bitbucket/trv_chef/1
-python /Users/tosoth/Projects/bitbucket/trv_chef/i.py
+curl -sb -H "Accept: application/json" -u tosoth:123123 http://localhost:7990/rest/api/1.0/projects/TRVCHEF/repos/trv_chef/pull-requests > /tmp/pr
+
+curl -sb -H "Accept: application/json" -u tosoth:123123 http://localhost:7990/rest/api/1.0/projects/TRVCHEF/repos/trv_chef/pull-requests/1/activities > /tmp/1
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+python $DIR/test_me.py
 
